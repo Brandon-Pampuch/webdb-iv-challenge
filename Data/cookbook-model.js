e0b5485bc1af662161bc45b8aa7666037ce6b61e
Brandon-Pampuch <brandonpampuch@gmail.com>
2019-06-06 17:11:55 -0500

    add getAllDishes query

---
+++ Data/cookbook-model.js
@@ -0,0 +1,18 @@
+const knex = require('knex')
+
+//configure knex
+
+const knexConfig = require('../knexfile') //has both tables
+  
+const db = knex(knexConfig.development);
+
+module.exports ={
+
+getAllDishes,
+
+    
+}
+
+function getAllDishes() {
+    return db('dishes')
+}
